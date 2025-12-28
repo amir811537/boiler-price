8153074d4d11b8b4880475e86f1d4a092153e3c6
amir811537 <amirhossain.bc.75@gmail.com>
2025-12-28 13:40:02 +0600

    added new link

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://price-app-backend.vercel.app",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://price-app-backend.vercel.app",
+  // baseURL: "http://localhost:5000",
 });
 
 export default api;
